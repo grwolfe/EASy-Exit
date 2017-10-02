bf1fa1d6bf4c7526a5dc3f08ba062a39636ccba9
Gregory Wolfe <grwolfe123@gmail.com>
2017-10-02 05:48:55 -0400

    This is the initial commit for the GUI focused development branch. It is intended to setup the basic interface to branch off of later in the project. All work on the user interface for the project should originate in this branch. There is also an included test of the Winforms GUI creator to see how the project works and how to use it.
     Changes to be committed:
    	new file:   .gitignore
    	new directory:   src/
    	new directory:   src/gui/
    	new directory:   tests/gui/

---
+++ tests/gui/winforms_test/winforms_test/myTest.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace winforms_test
+{
+    partial class myTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Hello_pushbutton = new System.Windows.Forms.Button();
+            this.MyTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Hello_pushbutton
+            // 
+            this.Hello_pushbutton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Hello_pushbutton.Location = new System.Drawing.Point(188, 99);
+            this.Hello_pushbutton.Name = "Hello_pushbutton";
+            this.Hello_pushbutton.Size = new System.Drawing.Size(107, 59);
+            this.Hello_pushbutton.TabIndex = 0;
+            this.Hello_pushbutton.Text = "Hello";
+            this.Hello_pushbutton.UseVisualStyleBackColor = true;
+            this.Hello_pushbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MyTextBox
+            // 
+            this.MyTextBox.Location = new System.Drawing.Point(49, 187);
+            this.MyTextBox.Name = "MyTextBox";
+            this.MyTextBox.ReadOnly = true;
+            this.MyTextBox.Size = new System.Drawing.Size(384, 22);
+            this.MyTextBox.TabIndex = 1;
+            this.MyTextBox.Tag = "myTextBox";
+            this.MyTextBox.TextChanged += new System.EventHandler(this.MyTextBox_TextChanged);
+            // 
+            // myTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 285);
+            this.Controls.Add(this.MyTextBox);
+            this.Controls.Add(this.Hello_pushbutton);
+            this.Name = "myTest";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Hello_pushbutton;
+        private System.Windows.Forms.TextBox MyTextBox;
+    }
+}
+
---
+++ tests/gui/winforms_test/winforms_test/myTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winforms_test
+{
+    public partial class myTest : Form
+    {
+        public myTest()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyTextBox.Text = "Hello!";
+        }
+
+        private void MyTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
