f7aaa3e83030c5ca273a7865954914b74ccdcc81
jona0thann <jnthn_moriarty@yahoo.com>
2017-11-03 18:39:32 -0400

    added code for hub to receive temp data; added LED support/cleaned-up node code

--- tests/nodes/nodeTempLogic_APImode.ino
+++ tests/nodes/nodeTempLogic_APImode.ino
@@ -1,4 +1,6 @@
 /*
+ Created by: Jonathan Moriarty
+ 
  Receives from the hardware serial, sends to software serial.
  Receives from software serial, sends to hardware serial.
 */
@@ -9,15 +11,21 @@
 
 #define rxPin 2
 #define txPin 3
+#define RED 6
+#define GREEN 7
+//32-bit hub/destination address
+#define HUB_ADDR_H 0x0013A200
+#define HUB_ADDR_L 0x409BD794
 
 SoftwareSerial xbeeSerial(rxPin, txPin); // RX(D2)(xbee out), TX(D3) (xbee in)
 int tmp102Address = 0x48;
+uint8_t temp = 0;
 // create the XBee object
 XBee xbee = XBee();
 uint8_t payload[1];
 
 // SH + SL Address of receiving XBee
-XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x409BD794);
+XBeeAddress64 addr64 = XBeeAddress64(HUB_ADDR_H, HUB_ADDR_L);
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
 
@@ -26,33 +34,48 @@ void setup() {
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   Serial.begin(115200);
+  Serial.println("Starting up!");
   // set the data rate for the SoftwareSerial port
   xbeeSerial.begin(115200);
   xbee.setSerial(xbeeSerial); 
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
 }
 
-float getTemperature(){
-  Wire.requestFrom(tmp102Address,2); 
-
+uint8_t getTemperature(){
+  Wire.requestFrom(tmp102Address, 2); 
   byte MSB = Wire.read();
   byte LSB = Wire.read();
-
   int TemperatureSum = ((MSB << 8) | LSB) >> 4; 
   float celsius = TemperatureSum*0.0625;
-  return celsius;
+  //Serial.print("Celsius: "); Serial.println(celsius);
+  float tempF = (1.8 * celsius) + 32;  //conver to farenheit
+  return tempF;
 }
-
+void blink(int pin) {
+    digitalWrite(pin, HIGH);
+    delay(1000);
+    digitalWrite(pin, LOW);
+}
 void loop() {
-    float celsius = getTemperature();
-    //Serial.print("Celsius: "); Serial.println(celsius);
-
-    uint8_t f = (1.8 * celsius) + 32;  
-    Serial.print("Fahrenheit: "); Serial.println(f);
-    payload[0] = f;
-    xbeeSerial.println(payload[0]);
+    temp = getTemperature();
+    Serial.print("Fahrenheit: "); Serial.println(temp);
+    payload[0] = temp;
+    //xbeeSerial.println(temp); //not needed
     xbee.send(zbTx);
-    
-    delay(3000);
+
+    //Control light colors based on temp. reading
+    if (temp >= 81) {
+      digitalWrite(GREEN, LOW);
+      blink(RED);
+    }
+    else if (temp < 81) {
+      digitalWrite(RED, LOW);
+      blink(GREEN);
+    }
+    delay(1000);
 }
 
 
