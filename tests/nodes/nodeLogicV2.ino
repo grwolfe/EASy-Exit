0f8700aadf2135cf39f8391ccc32df2f3a764907
jona0thann <jnthn_moriarty@yahoo.com>
2017-11-13 19:02:58 -0500

    mostly working (no interrupts/more functionality)

--- tests/nodes/nodeLogicV2.ino
+++ tests/nodes/nodeLogicV2.ino
@@ -5,187 +5,227 @@
  Receives from software serial, sends to hardware serial.
 */
 #include <SoftwareSerial.h>
-//#include <Printers.h>
-//#include <XBee.h>
-#include <Wire.h>   //header for tmp102
+#include <Wire.h>           //header for tmp102
 
-#define rxPin 2
-#define txPin 3
-#define RED 9
-#define GREEN 11
-//32-bit hub/destination address
-#define HUB_ADDR_H 0x0013A200
-#define HUB_ADDR_L 0x409BD794
+// Pin definitions:
 #define THRESHOLDTEMP 81
 #define BUFFERSIZE 200
+#define RED 9
+#define GREEN 11
+/* #define RED0
+ * #define RED1
+ * #define RED2
+ * #define RED3
+ * #define GREEN0
+ * #define GREEN1
+ * #define GREEN2
+ * #define GREEN3 
+ */
 
 const String MYID = "n8";
+const byte rxPin = 2;
+const byte txPin = 3;
+const int REDLEDS[7];
+const int GRNLEDS[7];
+int ledIndex = 0;
 SoftwareSerial xbeeSerial(rxPin, txPin); // RX(D2)(xbee out), TX(D3) (xbee in)
-int tmp102Addr0 = 0x48;
-int tmp102Addr1 = 0x49;
+int tmp102Addr0 = 0x48;   //(add0=GND)
+int tmp102Addr1 = 0x49;   //(add1=VCC)
 float temp0 = 0;
 float temp1 = 0;
+int temp0Stat = -1;
+int temp1Stat = -1;
 char frameBuff[BUFFERSIZE+1]; //buffersize + '\n'
 String frameToSend;
-// create the XBee object
-//XBee xbee = XBee();
-//uint8_t payload[1];
-
-// SH + SL Address of receiving XBee
-//XBeeAddress64 addr64 = XBeeAddress64(HUB_ADDR_H, HUB_ADDR_L);
-//ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
-//ZBTxStatusResponse txStatus = ZBTxStatusResponse();
+String nodeID = "n?";
+int instruction = -1;
 
 void setup() {
-  Wire.begin();
-  pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
-  Serial.begin(115200);
-  Serial.println("Starting up!");
-  // set the data rate for the SoftwareSerial port
-  xbeeSerial.begin(115200);
-  //xbee.setSerial(xbeeSerial); 
-  pinMode(RED, OUTPUT);
-  pinMode(GREEN, OUTPUT);
-  // initialize pins
-  digitalWrite(RED, LOW);
-  digitalWrite(GREEN, LOW);
-  //attachInterrupt(0, RxMessage_ISR, CHANGE);
-}
-
-//void RxMessage_ISR() {
-  //read rx buffer
-  //figure out what to do next
-  //if update request immediately send update to hub?
-//}
-
-void getTemperature(){
-  // get temp from first sensor
-  Wire.requestFrom(tmp102Addr0, 2); 
-  byte MSB = Wire.read();
-  byte LSB = Wire.read();
-  int tempSum = ((MSB << 8) | LSB) >> 4; 
-  float celsius = tempSum*0.0625;
-  //Serial.print("Celsius: "); Serial.println(celsius);
-  temp0 = (1.8 * celsius) + 32;  //conver to farenheit
-  
-  /* get temp from second sensor
-  Wire.requestFrom(tmp102Addr1, 2); 
-  MSB = Wire.read();
-  LSB = Wire.read();
-  tempSum = ((MSB << 8) | LSB) >> 4; 
-  celsius = tempSum*0.0625;
-  //Serial.print("Celsius: "); Serial.println(celsius);
-  temp1 = (1.8 * celsius) + 32;  //conver to farenheit
-*/
-}
-
-void blink(int pin) {
-    digitalWrite(pin, HIGH);
-    delay(1000);
-    digitalWrite(pin, LOW);
+	Wire.begin();
+	pinMode(rxPin, INPUT);
+	pinMode(txPin, OUTPUT);
+	Serial.begin(115200);
+	Serial.println("Starting up!");
+	// set the data rate for the SoftwareSerial port
+	xbeeSerial.begin(115200);
+	//set pin mode
+	pinMode(RED, OUTPUT);
+	pinMode(GREEN, OUTPUT);
+	/*
+	* for(ledIndex = 0; i < sizeof(REDLEDS); i++){
+	*    pinMode(REDLEDS[i], OUTPUT);
+	* {
+	* for(ledIndex = 0; i < sizeof(GRNLEDS); i++){
+	*    pinMode(GRNLEDS[i], OUTPUT);
+	* {
+	*/
+	// initialize pins
+	digitalWrite(RED, LOW);
+	digitalWrite(GREEN, LOW);
+	/*
+	* for(ledIndex = 0; i < sizeof(REDLEDS); i++){
+	*    digitalWrite(REDLEDS[i], LOW);
+	* {
+	* for(ledIndex = 0; i < sizeof(GRNLEDS); i++){
+	*    digitalWrite(GRNLEDS[i], LOW);
+	* {
+	*/
+	scanI2C();   //scan for temp sensors at startup
+	//xbeeSerial.println("Hello XCTU!");
 }
 
 void loop() {
+	nodeID = "n?";
+	recMessage();
+	if (MYID == nodeID) {
+		switch (instruction) {
+			case 0:                 //update request => frameToSend<nodeid, temp0Status, temp0, temp1Status, temp1>
+				getTemperature();
+				frameToSend = String(MYID) + ',' + String(temp0Stat) + ',' + String(temp0) + ',' + String(temp1Stat) + ',' + String(temp1) + '\n';
+				xbeeSerial.println(frameToSend);
+				break;
+
+			case 1:   //command
+				Serial.println("Got a command :P");
+				//...
+				break;
+
+			default:
+				Serial.println("Nada");
+				break;
+		}// end switch
+	}
+	else {
+		getTemperature();
+		Serial.print("Temp sensor 0(F): "); Serial.println(temp0);
+		Serial.print("Temp sensor 1(F): "); Serial.println(temp1);
+		delay(2500);
+		if (temp0 >= THRESHOLDTEMP) {
+			digitalWrite(GREEN, LOW);
+			/*
+			 * for(ledIndex = 0; i < sizeof(GRNLEDS); i++){
+				digitalWrite(GRNLEDS[i], LOW);
+			 */
+			onLed(RED);
+			/*for(ledIndex = 0; i < sizeof(REDLEDS); i++){
+				onLed(REDLEDS[i]);
+			  }
+			*/
+		}
+		else if (temp0 < THRESHOLDTEMP) {
+			digitalWrite(RED, LOW);
+			/*
+			 * for(ledIndex = 0; i < sizeof(REDLEDS); i++){
+				digitalWrite(REDLEDS[i], LOW);
+			 */
+			onLed(GREEN);
+			/*for(ledIndex = 0; i < sizeof(GRNLEDS); i++){
+				onLed(GRNLEDS[i]);
+			  }
+			*/
+		}
+	//  if (temp1 >= THRESHOLDTEMP) {
+	//    digitalWrite(GREEN, LOW);
+	//    blink(RED);
+	//  }
+	//  else if (temp1 < THRESHOLDTEMP) {
+	//    digitalWrite(RED, LOW);
+	//    blink(GREEN);
+	//  }
+		else {
+			digitalWrite(RED, LOW);
+			digitalWrite(GREEN, LOW);
+		}
+	}
+}// end loop
+
+void recMessage() {		// RECIEVE LOGIC 
+	memset(frameBuff, 0, sizeof(frameBuff));
+	int i = 0;
+	if (xbeeSerial.available() > 0) {
+		delay(2);
+		//while (xbeeSerial.available() > 0 && (xbeeSerial.peek() != '\n')) { 
+		while (xbeeSerial.peek() != '\n') { 
+			if ( xbeeSerial.available() )
+				frameBuff[i++] = xbeeSerial.read();
+		}
+		xbeeSerial.read();		//flushing '\n' from RX buffer
+		frameBuff[i] = '\0';    //null terminate frame buffer string
+		String frameStr(frameBuff);
+	   
+		// get indexes of delimiters (i.e. commas) within frame
+		int delim0 = frameStr.indexOf(',');
+		//int delim1 = frameStr(frameBuff).indexOf(',', delim0 + 1);    
+		
+		// Parse message: frameStr="nodeid, instruction" -> instr. 0 = update request; 1 = command
+		nodeID = frameStr.substring(0, delim0);
+		instruction = frameStr.substring(delim0 + 1).toInt(); 
+		Serial.print("nodeID: "); Serial.println(nodeID); Serial.print("instruction: "); Serial.println(instruction); 
+		Serial.print("Whole packet:"); Serial.print(frameStr);
+	}
+}// end recMessage()
+
+void scanI2C() {
 // ##### I2C scanner ######
-  byte error, address;
-  int nDevices = 0;
-  Serial.println(); Serial.println("Scanning...");
-  for(address = 0x48; address < 0x4C; address++ )
-  {
-    // The i2c_scanner uses the return value of the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    if (error == 0) {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.print(address,HEX); Serial.println("!");
-      nDevices++;
-    }
-    else if (error==4) {
-      Serial.print("Unknown error at address 0x");
-      if (address<16) {
-        Serial.print("0");
-      }
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0) {
-    Serial.println("No I2C devices found\n");
-  }
+	byte error, address;
+	int nDevices = 0;
+	Serial.println(); Serial.println("Scanning...");
+	for(address = 0x48; address < 0x4C; address++ ) {
+// The i2c_scanner uses the return value of the Write.endTransmisstion to see if
+// a device did acknowledge to the address.
+		Wire.beginTransmission(address);
+		error = Wire.endTransmission();
+		if (address==0x48)
+			temp0Stat = error;
+		if (address==0x49)
+			temp1Stat = error;
+
+		if (error == 0) {
+			Serial.print("I2C device found at address 0x");
+			if (address<16) 
+				Serial.print("0");
+			Serial.print(address,HEX); Serial.println("!");
+			nDevices++;
+		}
+		else if (error==4) {
+			Serial.print("Unknown error at address 0x");
+			if (address<16) 
+				Serial.print("0");
+			Serial.println(address,HEX);
+		}    
+	}// end for loop
+	if (nDevices == 0)
+		Serial.println("No I2C devices found\n");
+}
 // ### end I2C scanner ####
 
-//  ##### RECIEVE LOGIC #####
-  memset(frameBuff, 0, sizeof(frameBuff));
-  int i = 0;
-  if (xbeeSerial.available()) {
-    while (xbeeSerial.available() > 0) { 
-      frameBuff[i++] = xbeeSerial.read();
-    }
-      frameBuff[i] = '\0';    //null terminate frame buffer string
-      String frameStr(frameBuff);
-      
-      // 1) identify node => n<node#>(i.e. n0-n5)
-      // 2) parse out instruction
-      // 3/4) send response => frameToSend<nodeid, temp0Status, temp0, temp1Status, temp1>
-      //OR carryout command
-      
-      // get indexes of delimiters (i.e. commas) within frame
-      int delim0 = frameStr.indexOf(',');
-      //int delim1 = frameStr(frameBuff).indexOf(',', delim0 + 1);    
-      
-      // frameStr="nodeid, instruction" -> instr. 0 = update request; 1 = command
-      String nodeID = frameStr.substring(0, delim0);
-      int instruction = frameStr.substring(delim0 + 1).toInt(); 
-      Serial.print("nodeID: "); Serial.println(nodeID); Serial.print("instruction: "); Serial.println(instruction);
-      
-      if (MYID == nodeID) {
-        switch (instruction) {
-          case 0:   //update request => frameToSend<nodeid, temp0Status, temp0, temp1Status, temp1>
-            getTemperature();
-            Serial.print("Temp sensor 0(F): "); Serial.println(temp0);
-            //Serial.print("Temp sensor 1(F): "); Serial.println(temp1);
-            //Control light colors based on temp. reading
-            
-            if (temp0 >= THRESHOLDTEMP) {
-              digitalWrite(GREEN, LOW);
-              blink(RED);
-            }
-            else if (temp0 < THRESHOLDTEMP) {
-              digitalWrite(RED, LOW);
-              blink(GREEN);
-            }
-            delay(1000);
-            
-            //payload[0] = temp0&1;
-            //xbee.send(zbTx);
-                     
-            frameToSend = String(MYID) + ',' +/*temp0Stat +*/ String(temp0)/* + ',' + temp1Stat + String(temp1)*/;
-            xbeeSerial.println(frameToSend);
-            break;
-          
-          case 1:   //command
-            Serial.println("Got a command :P");
-            //...
-            break;
-          
-          default:
-            Serial.println("Nada");
-            break;
-        }//end switch
-      }
-      else {
-        //nothing
-      }
-  }//end .available()
-  else {
-    Serial.println("not available");
-    delay(2000);
-  }
+void getTemperature() {
+	// get temp from first sensor
+	Wire.requestFrom(tmp102Addr0, 2); 
+	byte MSB = Wire.read();
+	byte LSB = Wire.read();
+	int tempSum = ((MSB << 8) | LSB) >> 4; 
+	float celsius = tempSum*0.0625;
+	//Serial.print("Celsius: "); Serial.println(celsius);
+	temp0 = (1.8 * celsius) + 32;  //conver to farenheit
+
+// get temp from second sensor
+	Wire.requestFrom(tmp102Addr1, 2); 
+	MSB = Wire.read();
+	LSB = Wire.read();
+	tempSum = ((MSB << 8) | LSB) >> 4; 
+	celsius = tempSum*0.0625;
+	//Serial.print("Celsius: "); Serial.println(celsius);
+	temp1 = (1.8 * celsius) + 32;  //conver to farenheit
+
 }
 
-
+void onLed(int pin) {      // turn on led and wait
+    digitalWrite(pin, HIGH);
+    delay(1000);
+}
+void offLed(int pin) {      // turn on led and wait
+    digitalWrite(pin, LOW);
+    delay(1000);
+}
 
